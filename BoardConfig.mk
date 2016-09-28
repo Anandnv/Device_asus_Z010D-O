391288b5b592509c830a940b0d7a704e3503a329
audahadi <wan.audahadi@gmail.com>
2016-09-28 14:18:10 +0800

    rename to Z010D and add own modification

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,19 +16,25 @@
 # inherit from common msm8916-common
 -include device/asus/msm8916-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/asus/Z00L
+DEVICE_PATH := device/asus/Z010D
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := Z00L
+TARGET_OTA_ASSERT_DEVICE := Z010D,ASUS_Z010D,Z010DD,ASUS_Z010_2
 
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-TARGET_KERNEL_CONFIG := Z00L_defconfig
+TARGET_KERNEL_CONFIG := zc550kl-custom_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
+TARGET_INIT_VENDOR_LIB := libinit_msm8916
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8916
+
+# Use Snapdragon LLVM, if available		
+TARGET_USE_SDCLANG := true
+
 # inherit from the proprietary version
--include vendor/asus/Z00L/BoardConfigVendor.mk
+-include vendor/asus/Z010D/BoardConfigVendor.mk
